a265c5d8228d890e68cd6f65cc43fcddb09adefa
rcardozo <rodrigocardozo86@gmail.com>
2019-11-14 12:37:48 -0300

    Ejemplo basico de routing con Angular 8

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,28 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+/**Importamos las siguientes clases para trabajar con enrutamientos */
+import {RouterModule, Route} from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { AboutComponent } from './about/about.component';
+import { ServiciosComponent } from './servicios/servicios.component';
+
+/**Declaramos una constante con las rutas a utilizar*/
+const routes: Route[] = [
+  {path: 'about', component: AboutComponent},
+  {path: 'servicios', component: ServiciosComponent}
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AboutComponent,
+    ServiciosComponent
   ],
   imports: [
-    BrowserModule
-  ],
+    BrowserModule,
+    RouterModule.forRoot(routes)                       /**llamamos al metodo forRoot de la clase RouterModule y le pasamos como */
+  ],                                                   /**parametro el array de routes */
   providers: [],
   bootstrap: [AppComponent]
 })
